699f265596bd8e6d4a1bb1c690df2f573eb1eb40
Jrichards1024 <59896014+Jrichards1024@users.noreply.github.com>
2020-01-15 11:54:16 -0800

    Update isPositive.js

--- exercises/numbers/isPositive/isPositive.js
+++ exercises/numbers/isPositive/isPositive.js
@@ -1,11 +1,11 @@
-/**
- * Returns true if the given number is positive (greater than 0) and false otherwise.
- *
- * @param {number} num The input number
- * @returns {boolean} True if num is positive and false otherwise
- */
 function isPositive(num) {
-  return _____;
+    if (num > 0){
+    return true   
+    }
+    else {
+        return false;
+    }
+
 }
 
 if (require.main === module) {
