377d8d1b63c35400771247d9721be5fc7f04fb13
Jrichards1024 <59896014+Jrichards1024@users.noreply.github.com>
2020-01-15 11:57:16 -0800

    Update isNegative.js

--- exercises/numbers/isNegative/isNegative.js
+++ exercises/numbers/isNegative/isNegative.js
@@ -1,20 +1,23 @@
-/**
- * Returns true if the given number is negative (less than 0) and false otherwise.
- *
- * @param {number} num The input number
- * @returns {boolean} True if num is negative and false otherwise
- */
 function isNegative(num) {
-  return _____;
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for isNegative:');
-
-  console.log(isNegative(0) === false);
-  console.log(isNegative(1) === false);
-  console.log(isNegative(-1) === true);
-  console.log(isNegative(-100.20) === true);
-}
-
-module.exports = isNegative;
+    if (num > 0){
+        return false   
+    }
+    else if (num === 0) {
+        return false
+    }
+    else {
+        return true;
+    }
+    
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for isNegative:');
+  
+    console.log(isNegative(0) === false);
+    console.log(isNegative(1) === false);
+    console.log(isNegative(-1) === true);
+    console.log(isNegative(-100.20) === true);
+  }
+  
+  module.exports = isNegative;
