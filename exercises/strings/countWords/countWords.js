e4bc9bf75208a33de75d940214583a82b0acefde
Jrichards1024 <59896014+Jrichards1024@users.noreply.github.com>
2020-01-16 11:42:14 -0800

    Update countWords.js

--- exercises/strings/countWords/countWords.js
+++ exercises/strings/countWords/countWords.js
@@ -8,14 +8,21 @@
  */
 
 function countWords(string) {
-  // This is your job. :)
+  let newArray = string.split(' ')
+  console.log(newArray.length)
+  return newArray.length
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countWords:');
+}
+
+module.exports == countWords.js;
+  console.log(countWords("hello my name is") == 4);
+  console.log(countWords("joseph") == 1);
+  console.log(countWords("What is for dinner today?") == 5);
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
-}
 
 module.exports = countWords;
