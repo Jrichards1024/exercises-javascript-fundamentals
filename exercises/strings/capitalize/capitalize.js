d4729eca6d5e52f73abf1e6a5fcf80f31888b764
Jrichards1024 <59896014+Jrichards1024@users.noreply.github.com>
2020-01-16 11:28:19 -0800

    Update capitalize.js

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,15 +10,24 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
+function capitalize(string) {
+  newLetter = string[0].toUpperCase()
+  restOfLetter = string.slice(1)
+  return newLetter + restOfLetter
+  }
   // This is your job. :)
-}
+
 
 if (require.main === module) {
-  console.log('Running sanity checks for capitalize:');
+  console.log('Running sanity checks for capitalize');
+}
+module.exports == capitalize;
+  console.log(capitalize("hello") == "Hello");
+  console.log(capitalize("joseph") == "Joseph");
+  console.log(capitalize("beyoncé") == "Beyoncé");
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
-}
+
 
 module.exports = capitalize;
