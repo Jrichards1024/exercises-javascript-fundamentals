f037a9c7bd3812299285b09fa9774f176c6f5786
Jrichards1024 <59896014+Jrichards1024@users.noreply.github.com>
2020-01-16 13:23:18 -0800

    Update shortestWord.js

--- exercises/strings/shortestWord/shortestWord.js
+++ exercises/strings/shortestWord/shortestWord.js
@@ -7,15 +7,34 @@
  * @returns {string} The shortest word in the input string
  */
 
-function shortestWord(string) {
-  // This is your job. :)
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for shortestWord:');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
-}
-
-module.exports = shortestWord;
+  function shortestWord(string) {
+    let newArray = string.split(' ')
+    counter = 100
+    for (let element of newArray){
+      if (element.length < counter){
+      counter = element.length
+      }
+    }
+    for (let element of newArray){
+      if (element.length === counter){
+      return element
+      }
+    }
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for longestWord:');
+  }
+  
+  module.exports == shortestWord.js;
+    console.log(shortestWord("We are here") == "We");
+    console.log(shortestWord("I am Joseph") == "I");
+    console.log(shortestWord("What is for dinner today?") == "is");
+  
+    // Add your own sanity checks here.
+    // How else will you be sure your code does what you think it does?
+  
+  
+  module.exports = shortestWord;
+  
+  
